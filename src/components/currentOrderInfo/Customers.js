d9c8a6f8a826c86cef3bfdf9a410e775ebd99a12
Dan Berkeland <danberkeland@gmail.com>
2021-02-05 14:19:53 -0800

    backup

--- src/components/currentOrderInfo/Customers.js
+++ src/components/currentOrderInfo/Customers.js
@@ -1,46 +1,49 @@
-import React, { useContext, useState } from 'react';
-import { useEffect } from 'react/cjs/react.development';
+import React, { useContext, useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
-import { CurrentDataContext } from '../../dataContexts/CurrentDataContext';
+//import { CurrentDataContext } from '../../dataContexts/CurrentDataContext';
 import { CustomerContext } from '../../dataContexts/CustomerContext';
-import { OrdersContext } from '../../dataContexts/OrdersContext';
+//import { OrdersContext } from '../../dataContexts/OrdersContext';
 
-import { tomorrow } from '../../helpers/dateTimeHelpers'
-import { createRetailOrderCustomers } from '../../helpers/sortDataHelpers'
+//import { tomorrow } from '../../helpers/dateTimeHelpers'
+//import { createRetailOrderCustomers } from '../../helpers/sortDataHelpers'
 
 
 export const Customers = () => {
 
+    
     const { customers } = useContext(CustomerContext);
+    /*
     const { orders } = useContext(OrdersContext)
     const { chosen, setChosen, setDelivDate, orderTypeWhole } = useContext(CurrentDataContext)
-
+    */
     const [ customerGroup, setCustomerGroup ] = useState(customers)
 
-    
+    let orderTypeWhole = true
+
 
     useEffect(() => {
-        orderTypeWhole ? setCustomerGroup(customers) : setCustomerGroup(createRetailOrderCustomers(orders))
-    },[ customers, orderTypeWhole, orders ])
+        orderTypeWhole ? setCustomerGroup(customers) : setCustomerGroup(customers)
+    },[ customers ])
 
-    
+    /*
     const handleChange = e => {
         setChosen(e.target.value);
         setDelivDate(tomorrow())
         }
-
+    */
     return (
         <React.Fragment>
         <label>Customers:</label>
-        <select id = "customers" name="customers" value={chosen} onChange={handleChange}>
+        <select id = "customers" name="customers">
+        
             {customerGroup ? customerGroup.map((customer) => 
                     <option key = {uuidv4()} 
                             value={customer[2]}>
                                 {customer[2]}
                     </option>
             ) : ''};
-        </select>
+            </select> 
         </React.Fragment>
     );
 };
